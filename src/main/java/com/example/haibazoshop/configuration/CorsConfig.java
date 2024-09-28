95dd560cb20a64d946bd7cd56fe6eba32a201430
minhcopilot <21115053120333@sv.ute.udn.vn>
2024-09-28 09:32:40 +0700

    update cors

--- src/main/java/com/example/haibazoshop/configuration/CorsConfig.java
+++ src/main/java/com/example/haibazoshop/configuration/CorsConfig.java
@@ -1,13 +1,12 @@
 package com.example.haibazoshop.configuration;
 
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -16,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
