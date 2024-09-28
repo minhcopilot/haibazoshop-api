656e6f7741b18100c8a46ffa961bf641008e1770
minhcopilot <21115053120333@sv.ute.udn.vn>
2024-09-28 12:06:30 +0700

    config cors for server client

--- src/main/java/com/example/haibazoshop/configuration/CorsConfig.java
+++ src/main/java/com/example/haibazoshop/configuration/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://haibazo-client.vercel.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
